4a82c60c1a8a490e16702c99eec4126d928e2551
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-08-20 13:42:37 +0300

    feat: InventoryManagers

--- Assets/GameAssets/Scripts/Shop/ShopItem.cs
+++ Assets/GameAssets/Scripts/Shop/ShopItem.cs
@@ -20,12 +20,27 @@ namespace GameAssets.Scripts.Shop
             itemNameText.text = item.itemName;
             itemPriceText.text = item.itemPrice.ToString();
             itemSpriteImage.sprite = item.itemSprite;
+
+            if (item.clothType == ClothType.Hood)
+            {
+                inventory = FindObjectOfType<HoodInventory>();
+            }
+            else if (item.clothType == ClothType.Torso)
+            {
+                inventory = FindObjectOfType<TorsoInventory>();
+            }
+            
         }
         
         // Inventory için FindObjectOfType atılabilir
         public void BuyItem()
         {
             inventory.options.Add(item);
+        }
+
+        public void SellItem()
+        {
+            inventory.options.Remove(item);
         }
     }
 }
